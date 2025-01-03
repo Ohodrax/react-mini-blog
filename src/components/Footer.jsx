043b0ebd4bf89639da22e49011c0aeb25ebf8600
Ohodrax <rodrigo.cassiano00@gmail.com>
2025-01-03 12:06:29 -0300

    last lesson 131

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css";
+
+import React from 'react'
+
+const Footer = () => {
+    return <footer className={styles.footer}>
+        <h3>Escreva sobre o que você tem interesse!</h3>
+        <p>Mini Blog &copy; 2025</p>
+    </footer>
+}
+
+export default Footer
