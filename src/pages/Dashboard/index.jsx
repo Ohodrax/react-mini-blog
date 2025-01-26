e991e13ac7cacd2dd6b112be346798efb19a4d5d
Ohodrax <rodrigo.cassiano00@gmail.com>
2025-01-26 11:42:56 -0300

    last lesson 159

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,13 +2,30 @@ import { Link } from "react-router-dom";
 import styles from "./Dashboard.module.css";
 
 import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocument } from "../../hooks/useFetchDocuments";
 
 const Dashboard = () => {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
-  )
+	const {user} = useAuthValue();
+	const uid = user.uid
+
+	const posts = [];
+
+	return (
+		<div>
+			<h2>Dashboard</h2>
+			<p>Gerencie os seus posts</p>
+			{posts && posts.length === 0 ? (
+				<div className={styles.noposts}>
+					<p>Não foram encontrados posts</p>
+					<Link to="/posts/create" className="btn">Criar primeiro post</Link>
+				</div>
+			) : (
+				<div>
+					<p>Tem posts!</p>
+				</div>
+			)}
+		</div>
+	)
 }
 
 export default Dashboard
